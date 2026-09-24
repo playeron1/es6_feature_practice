726d33f7175e35987bb25319429b25106f7df8b9
playeron1 <krishnshah2003@gmail.com>
2026-09-24 21:15:32 +0530

    initiated project folder for practice

---
+++ index.js
@@ -0,0 +1,7 @@
+const name = "Krish";
+const numbers = [1, 2, 3, 4, 5];
+
+const doubledNumbers = numbers.map((number) => number * 2);
+
+console.log(`Hello, ${name}!`);
+console.log("Doubled numbers:", doubledNumbers);
