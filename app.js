a9840635a5c077041da7cb07c4f038ae91413ae7
playeron1 <krishnshah2003@gmail.com>
2026-09-25 21:23:22 +0530

    tried reduce functions

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 import students from "./index.js";
 console.log(JSON.stringify(students));
-let total =students.reduce((sum,current)=> acc+current,0);
+let marks = students[0].marks;
+let total = marks.reduce((sum,current)=> sum+current,0);
 students.forEach(element => {
-    console.log(students);
+    console.log(element);
 });
 console.log(total);
+let avg = total/marks.length;
+console.log(avg);
+function AverageCal(marks){
+    total = marks.reduce((sum,curr)=>sum+curr,0);
+    return total/marks.length;
+}
+console.log(`Avg Marks for Akash: ${AverageCal(students[0].marks)}`);
+console.log(`Avg Marks for Stud2: ${AverageCal(students[1].marks)}`);
+console.log(`Avg Marks for Stud3: ${AverageCal(students[2].marks)}`);
+
 
