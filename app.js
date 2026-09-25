a0f8079f35e1d7befafc802c59ab382719cf2ba4
playeron1 <krishnshah2003@gmail.com>
2026-09-25 21:14:17 +0530

    working on arrays

--- app.js
+++ app.js
@@ -1,2 +1,8 @@
 import students from "./index.js";
 console.log(JSON.stringify(students));
+let total =students.reduce((sum,current)=> acc+current,0);
+students.forEach(element => {
+    console.log(students);
+});
+console.log(total);
+
