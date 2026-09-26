1f2822b2e4a14d40e15a1eb0c51c90ec195823d9
playeron1 <krishnshah2003@gmail.com>
2026-09-26 13:31:30 +0530

    continuing with map function

--- app.js
+++ app.js
@@ -9,11 +9,15 @@ console.log(total);
 let avg = total/marks.length;
 console.log(avg);
 function AverageCal(marks){
-    total = marks.reduce((sum,curr)=>sum+curr,0);
+    const total = marks.reduce((sum,curr)=>sum+curr,0);
     return total/marks.length;
 }
 console.log(`Avg Marks for Akash: ${AverageCal(students[0].marks)}`);
 console.log(`Avg Marks for Stud2: ${AverageCal(students[1].marks)}`);
 console.log(`Avg Marks for Stud3: ${AverageCal(students[2].marks)}`);
 
-
+const results = students.map((student) => ({
+    ...student,
+    avg: AverageCal(student.marks)
+}));
+console.log(results);
